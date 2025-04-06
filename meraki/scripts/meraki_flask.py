a5035d63d341a1dc7a6b3d313c5ef4d6b83a32f3
Hunter Treadway <hunter@Hunters-Laptop.local>
2025-04-06 21:14:55 +0100

    Wrote a sample flask server to get meraki webhooks... except I haven't actually created a webhook, but used curl to test the idea

---
+++ meraki/scripts/meraki_flask.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/meraki-webhook', methods=['POST'])
+def meraki_alert():
+    data = request.get_json()
+
+    print("RECEIVED MERAKI ALERT!!!")
+    print(data) # logs full JSON payload
+
+        # Optional: parse specific fields
+    alert_type = data.get('alertType')
+    device_name = data.get('deviceName')
+    time = data.get('occurredAt')
+
+    print(f"[{time}] {alert_type} on {device_name}")
+
+    return jsonify({"status": "received"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2000)
+
+
